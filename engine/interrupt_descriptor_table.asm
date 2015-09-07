da5006ed43151b5819e94d8f3994a9f847894d31
Andrzej Adamczyk <akasei@wataha.net>
2015-09-07 22:07:24 +0200

    debug
    mały misz masz, ale działa !

--- engine/interrupt_descriptor_table.asm
+++ engine/interrupt_descriptor_table.asm
@@ -15,7 +15,7 @@
 [BITS 64]
 
 variable_idt_structure:
-variable_interrupt_descriptor_table_limit	dw	0x1000	; rozmiar tablicy / do 512 rekordów
+variable_interrupt_descriptor_table_limit	dw	VARIABLE_MEMORY_PAGE_SIZE	; rozmiar tablicy / do 512 rekordów
 variable_interrupt_descriptor_table_address	dq	VARIABLE_EMPTY
 
 ;===============================================================================
@@ -113,53 +113,434 @@ interrupt_descriptor_table:
 ;
 ; wszystkie rejestry zachowane
 itd_cpu_exception:
-	; wyświetl informację
+	push	r15
+	push	r14
+	push	r13
+	push	r12
+	push	r11
+	push	r10
+	push	r9
+	push	r8
+	push	rdx
+	push	rbp
+	push	rcx
+	push	rdi
+	push	rbx
+	push	rsi
+	push	rax
+
+	call	cyjon_screen_cursor_lock
+
 	mov	rbx,	VARIABLE_COLOR_RED
-	mov	rcx,	-1	; wyświetl całą informację
+	mov	rcx,	-1
 	mov	rdx,	VARIABLE_COLOR_BACKGROUND_DEFAULT
 	mov	rsi,	text_cpu_exception
-	call	cyjon_screen_print_string	; plik: engine/screen.asm
-
-	; wyświetl rozamiar pozostałej pamięcie w ilości stron
-	mov	rax,	qword [variable_binary_memory_map_free_pages]
-	mov	rcx,	10	; system dziesiętny
-	mov	rdx,	VARIABLE_COLOR_BACKGROUND_DEFAULT
-	call	cyjon_screen_print_number
-
-	; przejdź do następnej linii
-	mov	rsi,	text_paragraph
 	call	cyjon_screen_print_string
 
-	; pobierz z stosu 6 wartości
-	mov	rcx,	5
-.loop:
-	; pobierz z stosu informacje
+	mov	rbx,	VARIABLE_COLOR_ORANGE
+
+	mov	rsi,	text_cpu_exception_rbx
+	call	cyjon_screen_print_string
+
 	pop	rax
-
-	; zachowaj licznik
-	push	rcx
-
-	; system liczbowy
 	mov	rcx,	16
-
-	; wyświetl wartość
+	mov	ch,	1
 	call	cyjon_screen_print_number
-	; przejdź do nastepnej linii
-	mov	rsi,	text_paragraph
+
+	mov	dword [variable_screen_cursor_xy],	23
+	mov	rbx,	qword [variable_screen_cursor_xy]
+	call	screen_cursor_set_xy
+
+	mov	rbx,	VARIABLE_COLOR_ORANGE
+
+	mov	rsi,	text_cpu_exception_rsi
 	call	cyjon_screen_print_string
 
-	; przywróć licznik
-	pop	rcx
-	; kontynuuj z kolejnymi wartościami
-	loop	.loop
+	pop	rax
+	call	cyjon_screen_print_number
 
-	; wstrzymaj przełączanie procesów
-	inc	qword [variable_multitasking_semaphore_process_table]
+	mov	rsi,	text_cpu_exception_rbx
+	call	cyjon_screen_print_string
 
-	; zatrzymaj dalsze wykonywanie kodu jądra
-	jmp	$
+	pop	rax
+	call	cyjon_screen_print_number
 
-text_cpu_exception	db	"Unhandled CPU exception! pages:", VARIABLE_ASCII_CODE_TERMINATOR
+	mov	dword [variable_screen_cursor_xy],	23
+	mov	rbx,	qword [variable_screen_cursor_xy]
+	call	screen_cursor_set_xy
+
+	mov	rbx,	VARIABLE_COLOR_ORANGE
+
+	mov	rsi,	text_cpu_exception_rdi
+	call	cyjon_screen_print_string
+
+	pop	rax
+	call	cyjon_screen_print_number
+
+	mov	rsi,	text_cpu_exception_rcx
+	call	cyjon_screen_print_string
+
+	pop	rax
+	call	cyjon_screen_print_number
+
+	mov	dword [variable_screen_cursor_xy],	23
+	mov	rbx,	qword [variable_screen_cursor_xy]
+	call	screen_cursor_set_xy
+
+	mov	rbx,	VARIABLE_COLOR_ORANGE
+
+	mov	rsi,	text_cpu_exception_rbp
+	call	cyjon_screen_print_string
+
+	pop	rax
+	call	cyjon_screen_print_number
+
+	mov	rsi,	text_cpu_exception_rdx
+	call	cyjon_screen_print_string
+
+	pop	rax
+	call	cyjon_screen_print_number
+
+	mov	dword [variable_screen_cursor_xy],	23
+	mov	rbx,	qword [variable_screen_cursor_xy]
+	call	screen_cursor_set_xy
+
+	mov	rbx,	VARIABLE_COLOR_ORANGE
+
+	mov	rsi,	text_cpu_exception_rsp
+	call	cyjon_screen_print_string
+
+	cmp	qword [rsp + 0x40],	VARIABLE_KERNEL_PHYSICAL_ADDRESS
+	ja	.no_code_error
+
+	; pomiń kod błędu
+	mov	rax,	qword [rsp + 0x60]
+
+	jmp	.error_code
+
+.no_code_error:
+	mov	rax,	qword [rsp + 0x58]
+
+.error_code:
+	call	cyjon_screen_print_number
+
+	mov	rsi,	text_cpu_exception_rip
+	call	cyjon_screen_print_string
+
+	mov	rax,	qword [rsp + 9 * 0x08]
+	call	cyjon_screen_print_number
+
+	mov	rsi,	text_cpu_exception_eflags
+	call	cyjon_screen_print_string
+
+	cmp	qword [rsp + 0x40],	VARIABLE_KERNEL_PHYSICAL_ADDRESS
+	ja	.no_code_error1
+
+	; pomiń kod błędu
+	mov	rax,	qword [rsp + 11 * 0x08]
+
+	jmp	.error_code1
+
+.no_code_error1:
+	mov	rax,	qword [rsp + 10 * 0x08]
+
+.error_code1:
+	mov	rcx,	15
+	mov	r8,	4
+
+	bt	ax,	cx
+	jnc	.noID
+
+	xchg	rcx,	r8
+	mov	rsi,	text_cpu_exception_flag_id
+	call	cyjon_screen_print_string
+	xchg	rcx,	r8
+
+.noID:
+	dec	rcx
+
+	bt	ax,	cx
+	jnc	.noVIP
+
+	xchg	rcx,	r8
+	mov	rsi,	text_cpu_exception_flag_vip
+	call	cyjon_screen_print_string
+	xchg	rcx,	r8
+
+.noVIP:
+	dec	rcx
+
+	bt	ax,	cx
+	jnc	.noVIF
+
+	xchg	rcx,	r8
+	mov	rsi,	text_cpu_exception_flag_vif
+	call	cyjon_screen_print_string
+	xchg	rcx,	r8
+
+.noVIF:
+	dec	rcx
+
+	bt	ax,	cx
+	jnc	.noAC
+
+	xchg	rcx,	r8
+	mov	rsi,	text_cpu_exception_flag_ac
+	call	cyjon_screen_print_string
+	xchg	rcx,	r8
+
+.noAC:
+	dec	rcx
+
+	bt	ax,	cx
+	jnc	.noVM
+
+	xchg	rcx,	r8
+	mov	rsi,	text_cpu_exception_flag_vm
+	call	cyjon_screen_print_string
+	xchg	rcx,	r8
+
+.noVM:
+	dec	rcx
+
+	bt	ax,	cx
+	jnc	.noRF
+
+	xchg	rcx,	r8
+	mov	rsi,	text_cpu_exception_flag_rf
+	call	cyjon_screen_print_string
+	xchg	rcx,	r8
+
+.noRF:
+	dec	rcx
+
+	bt	ax,	cx
+	jnc	.noNT
+
+	xchg	rcx,	r8
+	mov	rsi,	text_cpu_exception_flag_nt
+	call	cyjon_screen_print_string
+	xchg	rcx,	r8
+
+.noNT:
+	dec	rcx
+
+	bt	ax,	cx
+	jnc	.noOF
+
+	xchg	rcx,	r8
+	mov	rsi,	text_cpu_exception_flag_of
+	call	cyjon_screen_print_string
+	xchg	rcx,	r8
+
+.noOF:
+	dec	rcx
+
+	bt	ax,	cx
+	jnc	.noDF
+
+	xchg	rcx,	r8
+	mov	rsi,	text_cpu_exception_flag_df
+	call	cyjon_screen_print_string
+	xchg	rcx,	r8
+
+.noDF:
+	dec	rcx
+
+	bt	ax,	cx
+	jnc	.noIF
+
+	xchg	rcx,	r8
+	mov	rsi,	text_cpu_exception_flag_if
+	call	cyjon_screen_print_string
+	xchg	rcx,	r8
+
+.noIF:
+	dec	rcx
+
+	bt	ax,	cx
+	jnc	.noTF
+
+	xchg	rcx,	r8
+	mov	rsi,	text_cpu_exception_flag_tf
+	call	cyjon_screen_print_string
+	xchg	rcx,	r8
+
+.noTF:
+	dec	rcx
+
+	bt	ax,	cx
+	jnc	.noSF
+
+	xchg	rcx,	r8
+	mov	rsi,	text_cpu_exception_flag_sf
+	call	cyjon_screen_print_string
+	xchg	rcx,	r8
+
+.noSF:
+	dec	rcx
+
+	bt	ax,	cx
+	jnc	.noZF
+
+	xchg	rcx,	r8
+	mov	rsi,	text_cpu_exception_flag_zf
+	call	cyjon_screen_print_string
+	xchg	rcx,	r8
+
+.noZF:
+	dec	rcx
+
+	bt	ax,	cx
+	jnc	.noAF
+
+	xchg	rcx,	r8
+	mov	rsi,	text_cpu_exception_flag_af
+	call	cyjon_screen_print_string
+	xchg	rcx,	r8
+
+.noAF:
+	dec	rcx
+
+	bt	ax,	cx
+	jnc	.noPF
+
+	xchg	rcx,	r8
+	mov	rsi,	text_cpu_exception_flag_pf
+	call	cyjon_screen_print_string
+	xchg	rcx,	r8
+
+.noPF:
+	dec	rcx
+
+	bt	ax,	cx
+	jnc	.noCF
+
+	xchg	rcx,	r8
+	mov	rsi,	text_cpu_exception_flag_cf
+	call	cyjon_screen_print_string
+	xchg	rcx,	r8
+
+.noCF:
+	mov	rcx,	16
+	mov	ch,	1
+	mov	rsi,	text_cpu_exception_r8
+	call	cyjon_screen_print_string
+
+	pop	rax
+	call	cyjon_screen_print_number
+
+	mov	dword [variable_screen_cursor_xy],	23
+	mov	rbx,	qword [variable_screen_cursor_xy]
+	call	screen_cursor_set_xy
+
+	mov	rbx,	VARIABLE_COLOR_ORANGE
+
+	mov	rsi,	text_cpu_exception_r12
+	call	cyjon_screen_print_string
+
+	pop	rax
+	call	cyjon_screen_print_number
+
+	mov	rsi,	text_cpu_exception_r9
+	call	cyjon_screen_print_string
+
+	pop	rax
+	call	cyjon_screen_print_number
+
+	mov	dword [variable_screen_cursor_xy],	23
+	mov	rbx,	qword [variable_screen_cursor_xy]
+	call	screen_cursor_set_xy
+
+	mov	rbx,	VARIABLE_COLOR_ORANGE
+
+	mov	rsi,	text_cpu_exception_r13
+	call	cyjon_screen_print_string
+
+	pop	rax
+	call	cyjon_screen_print_number
+
+	mov	rsi,	text_cpu_exception_r10
+	call	cyjon_screen_print_string
+
+	pop	rax
+	call	cyjon_screen_print_number
+
+	mov	dword [variable_screen_cursor_xy],	23
+	mov	rbx,	qword [variable_screen_cursor_xy]
+	call	screen_cursor_set_xy
+
+	mov	rbx,	VARIABLE_COLOR_ORANGE
+
+	mov	rsi,	text_cpu_exception_r14
+	call	cyjon_screen_print_string
+
+	pop	rax
+	call	cyjon_screen_print_number
+
+	mov	rsi,	text_cpu_exception_r11
+	call	cyjon_screen_print_string
+
+	pop	rax
+	call	cyjon_screen_print_number
+
+	mov	dword [variable_screen_cursor_xy],	23
+	mov	rbx,	qword [variable_screen_cursor_xy]
+	call	screen_cursor_set_xy
+
+	mov	rbx,	VARIABLE_COLOR_ORANGE
+
+	mov	rsi,	text_cpu_exception_r15
+	call	cyjon_screen_print_string
+
+	pop	rax
+	call	cyjon_screen_print_number
+
+	mov	rsi,	text_paragraph
+	call	cyjon_screen_print_string
+	call	cyjon_screen_print_string
+
+	call	cyjon_screen_cursor_unlock
+
+	sti
+
+	jmp	irq64_process.process_kill
+
+text_cpu_exception	db	VARIABLE_ASCII_CODE_ENTER, ":: An unexpected error occurred, the program has been closed.", VARIABLE_ASCII_CODE_ENTER, VARIABLE_ASCII_CODE_NEWLINE, VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_rax		db	" RAX 0x", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_rbx		db	VARIABLE_ASCII_CODE_ENTER, VARIABLE_ASCII_CODE_NEWLINE, " RBX 0x", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_rcx		db	VARIABLE_ASCII_CODE_ENTER, VARIABLE_ASCII_CODE_NEWLINE, " RCX 0x", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_rdx		db	VARIABLE_ASCII_CODE_ENTER, VARIABLE_ASCII_CODE_NEWLINE, " RDX 0x", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_rsi		db	"  RSI 0x", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_rdi		db	"  RDI 0x", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_rbp		db	"  RBP 0x", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_rsp		db	"  RSP 0x", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_eflags	db	"  EFLAGS:", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_flag_id	db	" ID", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_flag_vip	db	" VIP", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_flag_vif	db	" VIF", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_flag_ac	db	" AC", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_flag_vm	db	" VM", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_flag_rf	db	" RF", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_flag_nt	db	" NT", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_flag_of	db	" OF", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_flag_df	db	" DF", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_flag_if	db	" IF", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_flag_tf	db	" TF", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_flag_sf	db	" SF", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_flag_zf	db	" ZF", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_flag_af	db	" AF", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_flag_pf	db	" PF", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_flag_cf	db	" CF", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_r8		db	VARIABLE_ASCII_CODE_ENTER, VARIABLE_ASCII_CODE_NEWLINE, VARIABLE_ASCII_CODE_NEWLINE, " R8  0x", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_r12		db	"  R12 0x", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_r9		db	VARIABLE_ASCII_CODE_ENTER, VARIABLE_ASCII_CODE_NEWLINE, " R9  0x", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_r13		db	"  R13 0x", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_r10		db	VARIABLE_ASCII_CODE_ENTER, VARIABLE_ASCII_CODE_NEWLINE, " R10 0x", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_r14		db	"  R14 0x", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_r11		db	VARIABLE_ASCII_CODE_ENTER, VARIABLE_ASCII_CODE_NEWLINE, " R11 0x", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_r15		db	"  R15 0x", VARIABLE_ASCII_CODE_TERMINATOR
+text_cpu_exception_rip		db	VARIABLE_ASCII_CODE_ENTER, VARIABLE_ASCII_CODE_NEWLINE, VARIABLE_ASCII_CODE_NEWLINE, " RIP 0x", VARIABLE_ASCII_CODE_TERMINATOR
 
 ;===============================================================================
 ; procedura podstawowej obsługi przerwania sprzętowego
