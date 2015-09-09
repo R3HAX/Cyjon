95fb0d244a1bbda060c550e8afe461a7fa1a009f
Andrzej Adamczyk <akasei@wataha.net>
2015-09-09 20:05:20 +0200

    Update process.asm

--- engine/process.asm
+++ engine/process.asm
@@ -86,129 +86,176 @@ cyjon_process_init:
 	; zamień rozmiar pliku na strony
 	shr	rcx,	12
 
-;	; przygotuj przestrzeń pod proces
-;	mov	rax,	VARIABLE_MEMORY_HIGH_VIRTUAL_ADDRESS
-;	mov	rbx,	0x07	; flagi: Użytkownik, 4 KiB, Odczyt/Zapis, Dostępna
-;	mov	r11,	cr3	; tablica PML4 jądra systemu
-;	call	cyjon_page_map_logical_area	; wykonaj
-;
-;	; załaduj plik do pamięci
-;	mov	rsi,	qword [rdi]	; numer pierwszego bloku danych pliku
-;	mov	rdi,	VARIABLE_MEMORY_HIGH_REAL_ADDRESS
-;	call	cyjon_virtual_file_system_read_file
-;
-;	; przygotuj miejsce pod stos procesu
-;	mov	rax,	VARIABLE_MEMORY_HIGH_VIRTUAL_ADDRESS + VARIABLE_MEMORY_HIGH_VIRTUAL_ADDRESS - 0x1000	; ostatnie 4 KiB pamięci logicznej
-;	mov	rbx,	0x07	; flagi: Użytkownik, 4 KiB, Odczyt/Zapis, Dostępna
-;	mov	rcx,	1	; rozmiar stosu, jedna strona (4096 Bajtów)
-;	mov	r11,	cr3	; tablica PML4 jądra systemu
-;	call	cyjon_page_map_logical_area	; wykonaj
-;
-;	; przygotuj miejsce dla tablicy PML4 procesu
-;	call	cyjon_page_allocate
-;
-;	; zapamiętaj
-;	push	rdi
-;
-;	; mapuj tablicę PML4 jądra do procesu
-;	mov	rsi,	cr3	; tablica PML4 jądra systemu
-;	mov	rcx,	512	; 512 rekordów
-;	rep	movsq	; kopiuj
-;
-;	; załaduj tablicę PML4 procesu
-;	mov	r11,	qword [rsp]
-;
-;	; usuń stos kontekstu jądra, zostanie utworzony nowy dla procesu
-;	mov	qword [r11 + 0x07F8],	VARIABLE_EMPTY
-;
-;	; utwórz stos kontekstu procesu
-;	mov	rax,	VARIABLE_MEMORY_HIGH_VIRTUAL_ADDRESS - 0x1000	; ostatnie 4 KiB Low Memory
-;	mov	rcx,	1	; jedna strona o rozmiarze 4 KiB
-;	mov	rbx,	0x03	; ustaw flagi 4 KiB, Administrator, 4 KiB, Odczyt/Zapis, Dostępna
-;	call	cyjon_page_map_logical_area	; wykonaj
-;
-;	; odłóż na stos kontekstu procesu spreparowane dane powrotu z planisty
-;	mov	rdi,	qword [r8]
-;	and	di,	0xFFF0	; usuń właściwości strony z adresu
-;
-;	; wyczyść stos kontekstu procesu
-;	call	cyjon_page_clear
-;
-;	; przesuń wskaźnik na spreparowany wskaźnik szczytu stosu kontekstu procesu
-;	add	rdi,	0x1000 - ( 5 * 0x08 )
-;
-;	; odstaw na stos kontekstu procesu spreparowane dane powrotu z przerwania IRQ0
-;
-;	; RIP
-;	mov	rax,	VARIABLE_MEMORY_HIGH_REAL_ADDRESS
-;	stosq	; zapisz
-;
-;	; CS
-;	mov	rax,	0x18 | 3	; +3 typ: proces
-;	stosq	; zapisz
-;
-;	; EFLAGS
-;	mov	rax,	0x246
-;	stosq	; zapisz
-;
-;	; RSP
-;	mov	rax,	VARIABLE_EMPTY
-;	stosq	; zapisz
-;
-;	; DS
-;	mov	rax,	0x20 | 3	; +3 typ: proces
-;	stosq	; zapisz
-;
-;	; załaduj adres tablicy PML4 procesu
-;	mov	rax,	qword [rsp]
-;
-.free:
-;	; oczekuj na wolne miejsce w tablicy procesów
-;	cmp	qword [variable_process_table_count],	VARIABLE_PROCESS_LIMIT
-;	je	.free
-;
+	; przygotuj przestrzeń pod proces
+	mov	rax,	VARIABLE_MEMORY_HIGH_VIRTUAL_ADDRESS
+	mov	rbx,	0x07	; flagi: Użytkownik, 4 KiB, Odczyt/Zapis, Dostępna
+	mov	r11,	cr3	; tablica PML4 jądra systemu
+	call	cyjon_page_map_logical_area	; wykonaj
+
+	; załaduj plik do pamięci
+	mov	rsi,	qword [rdi]	; numer pierwszego bloku danych pliku
+	mov	rdi,	VARIABLE_MEMORY_HIGH_REAL_ADDRESS
+	call	cyjon_virtual_file_system_read_file
+
+	; przygotuj miejsce pod stos procesu
+	mov	rax,	VARIABLE_MEMORY_HIGH_VIRTUAL_ADDRESS + VARIABLE_MEMORY_HIGH_VIRTUAL_ADDRESS - 0x1000	; ostatnie 4 KiB pamięci logicznej
+	mov	rbx,	0x07	; flagi: Użytkownik, 4 KiB, Odczyt/Zapis, Dostępna
+	mov	rcx,	1	; rozmiar stosu, jedna strona (4096 Bajtów)
+	mov	r11,	cr3	; tablica PML4 jądra systemu
+	call	cyjon_page_map_logical_area	; wykonaj
+
+	; przygotuj miejsce dla tablicy PML4 procesu
+	call	cyjon_page_allocate
+
+	; zapamiętaj
+	push	rdi
+
+	; mapuj tablicę PML4 jądra do procesu
+	mov	rsi,	cr3	; tablica PML4 jądra systemu
+	mov	rcx,	512	; 512 rekordów
+	rep	movsq	; kopiuj
+
+	; załaduj tablicę PML4 procesu
+	mov	r11,	qword [rsp]
+
+	; usuń stos kontekstu jądra, zostanie utworzony nowy dla procesu
+	mov	qword [r11 + 0x07F8],	VARIABLE_EMPTY
+
+	; utwórz stos kontekstu procesu
+	mov	rax,	VARIABLE_MEMORY_HIGH_VIRTUAL_ADDRESS - 0x1000	; ostatnie 4 KiB Low Memory
+	mov	rcx,	1	; jedna strona o rozmiarze 4 KiB
+	mov	rbx,	0x03	; ustaw flagi 4 KiB, Administrator, 4 KiB, Odczyt/Zapis, Dostępna
+	call	cyjon_page_map_logical_area	; wykonaj
+
+	; odłóż na stos kontekstu procesu spreparowane dane powrotu z planisty
+	mov	rdi,	qword [r8]
+	and	di,	0xFFF0	; usuń właściwości strony z adresu
+
+	; wyczyść stos kontekstu procesu
+	call	cyjon_page_clear
+
+	; przesuń wskaźnik na spreparowany wskaźnik szczytu stosu kontekstu procesu
+	add	rdi,	0x1000 - ( 5 * 0x08 )
+
+	; odstaw na stos kontekstu procesu spreparowane dane powrotu z przerwania IRQ0
+
+	; RIP
+	mov	rax,	VARIABLE_MEMORY_HIGH_REAL_ADDRESS
+	stosq	; zapisz
+
+	; CS
+	mov	rax,	0x18 | 3	; +3 typ: proces
+	stosq	; zapisz
+
+	; EFLAGS
+	mov	rax,	0x246
+	stosq	; zapisz
+
+	; RSP
+	mov	rax,	VARIABLE_EMPTY
+	stosq	; zapisz
+
+	; DS
+	mov	rax,	0x20 | 3	; +3 typ: proces
+	stosq	; zapisz
+
+	; załaduj adres tablicy PML4 procesu
+	mov	rax,	qword [rsp]
+
 .wait:
-;	; sprawdź czy tablica procesów jest dostępna
-;	cmp	byte [variable_multitasking_semaphore_process_table],	0x01
-;	je	.wait	; jeśli nie, czekaj
-;
-;	; zablokuj tablice procesów
-;	mov	byte [variable_multitasking_semaphore_process_table],	0x01
-;
-;	; znajdź wolny rekord w tablicy procesów
-;	mov	rdi,	qword [variable_process_table_address]
-;
+	; sprawdź czy tablica procesów jest dostępna
+	cmp	byte [variable_process_serpentine_blocked],	0x01
+	je	.wait	; jeśli nie, czekaj
+
+	; zablokuj tablice procesów
+	mov	byte [variable_process_serpentine_blocked],	0x01
+
+	; znajdź wolny rekord w tablicy procesów
+	mov	rdi,	qword [variable_process_serpentine_start_address]
+
+	; pomiń nagłówek
+	add	rdi,	0x08
+
 .loop:
-;	; szukaj wolnego rekordu w tablicy
-;	cmp	qword [rdi],	VARIABLE_EMPTY
-;	je	.found
-;
-;	; przesuń wskaźnik na następny rekord
-;	add	rdi,	0x10
-;
-;	; szukaj dalej
-;	jmp	.loop
-;
+	; flaga
+	xor	bx,	bx
+
+	; licznik rekordów
+	xor	rcx,	rcx
+
+.next:
+	; przesuń na następny rekord
+	add	rdi,	STATIC_PROCESS_RECORD.SIZE
+
+	; rekord zajęty
+	inc	rcx
+
+	; koniec rekordów w części serpentyny?
+	cmp	rcx,	STATIC_PROCESS_RECORDS_PER_PAGE
+	jb	.in_page
+
+	; zładuj adres kolejnej części serpentyny
+	mov	rdi,	qword [rdi]
+	; pomiń nagłówek
+	add	rdi,	0x08
+
+	; zresetuj licznik rekordów na część serpentyny
+	xor	rcx,	rcx
+
+.in_page:
+	; sprawdź czy rekord jest aktywny
+	bt	word [rdi + STATIC_PROCESS_RECORD.FLAGS],	bx
+	jc	.next	; jeśli tak
+
+	; flaga
+	inc	bx
+	; sprawdź czy rekord jest dostepny do wykorzystania
+	bt	word [rdi + STATIC_PROCESS_RECORD.FLAGS],	bx
+	jc	.loop
+
 .found:
-;	; oblicz identyfikator procesu
-;	mov	rax,	rdi
-;
-;	; wyczyść starszą część
-;	and	rax,	0x0000000000000fff
-;	shr	rax,	4	; / 0x10
-;
-;	; zapamiętaj identyfikator procesu
-;	xchg	rax,	qword [rsp]
-;
-;	; zapisz adres tablicy PML4 procesu do rekordu
-;	stosq
-;	
-;	; zapisz adres szczytu stosu kontekstu procesu do rekordu
-;	mov	rax,	VARIABLE_MEMORY_HIGH_VIRTUAL_ADDRESS - (21 * 0x08)
-;	stosq
-;
-;	; zwiększ ilość rekordów/procesów przechowywanych w tablicy
-;	inc	qword [variable_process_table_count]
+	; pobierz dostępny identyfikator procesu
+	mov	rax,	qword [variable_process_pid_next]
+
+	; zachowaj
+	push	rax
+
+	; szukaj nastepnego wolnego
+	inc	rax
+
+	; sprawdź czy numer procesu jest dozwolony (modulo ROZMIAR_STRONY != 0)
+	mov	rcx,	VARIABLE_MEMORY_PAGE_SIZE
+	xor	rdx,	rdx
+	div	rcx
+
+	cmp	rdx,	VARIABLE_EMPTY
+	je	.pid
+
+	; następny
+	inc	rax
+
+.pid:
+	; zapisz
+	mov	qword [variable_process_pid_next],	rax
+
+	; przywróć
+	pop	rax
+
+	; zapamiętaj identyfikator procesu
+	xchg	rax,	qword [rsp]
+
+	; zapisz adres tablicy PML4 procesu do rekordu
+	stosq
+	
+	; zapisz adres szczytu stosu kontekstu procesu do rekordu
+	mov	rax,	VARIABLE_MEMORY_HIGH_VIRTUAL_ADDRESS - (21 * 0x08)
+	stosq
+
+	; zachowaj	
+	push	rdi
+
+	; zwiększ ilość rekordów/procesów przechowywanych w tablicy
+	inc	qword [variable_process_serpentine_record_count]
 ;
 ;	; odblokuj tablice procesów dla planisty
 ;	mov	byte [variable_multitasking_semaphore_process_table],	VARIABLE_EMPTY
