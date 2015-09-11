96f3766dace68affda9aff520e89d26b38b368e8
Andrzej Adamczyk <akasei@wataha.net>
2015-09-11 19:09:05 +0200

    Update make.bat

--- make.bat
+++ make.bat
@@ -5,12 +5,13 @@ nasm -f bin software\help.asm -o help.bin
 nasm -f bin software\uptime.asm -o uptime.bin
 nasm -f bin software\moko.asm -o moko.bin
 nasm -f bin software\ps.asm -o ps.bin
+nasm -f bin software\date.asm -o date.bin
 
 nasm -f bin kernel.asm -o kernel.bin
 
 nasm -f bin bootloader\stage2.asm -o stage2.bin
 nasm -f bin bootloader\stage1.asm -o build\disk.raw
 
-del /F /Q stage2.bin init.bin shell.bin help.bin login.bin uptime.bin moko.bin kernel.bin ps.bin
+del /F /Q stage2.bin init.bin shell.bin help.bin login.bin uptime.bin moko.bin kernel.bin ps.bin date.bin
 
 pause
