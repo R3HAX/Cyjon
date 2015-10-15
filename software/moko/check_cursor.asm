428b6059960892177fffbefb067588525ccab72a
Andrzej Adamczyk <akasei@wataha.net>
2015-10-15 20:35:30 +0200

    korekta kursora

---
+++ software/moko/check_cursor.asm
@@ -0,0 +1,42 @@
+; Copyright (C) 2013-2015 Wataha.net
+; All Rights Reserved
+;
+; LICENSE Creative Commons BY-NC-ND 4.0
+; See LICENSE.TXT
+;
+; Main developer:
+;	Andrzej (akasei) Adamczyk [e-mail: akasei from wataha.net]
+;-------------------------------------------------------------------------------
+
+; Use:
+; nasm - http://www.nasm.us/
+
+; 64 Bitowy kod programu
+[BITS 64]
+
+check_cursor:
+	push	rax
+
+	; sprawdź pozycję kolumny
+	mov	eax,	dword [variable_screen_size]
+	cmp	eax,	dword [variable_cursor_position]
+	jne	.column_ok
+
+	inc	qword [variable_line_print_start]
+	dec	dword [variable_cursor_position]
+
+.column_ok:
+	; sprawdź pozycję wiersza
+	mov	eax,	dword [variable_screen_size + 0x04]
+	sub	eax,	VARIABLE_INTERFACE_MENU_HEIGHT
+	cmp	eax,	dword [variable_cursor_position + 0x04]
+	jne	.row_ok
+
+	inc	qword [variable_document_line_start]
+	dec	dword [variable_cursor_position + 0x04]
+
+.row_ok:
+	pop	rax
+
+	; powrót z procedury
+	ret
